f5622ca98ee834564d26a51c846efef507d81fa3
LucasKendi <hiranolucas@gmail.com>
2020-05-31 15:02:37 -0300

    Add transfer controller to execute an exchange

---
+++ app/controllers/api/v1/transfers_controller.rb
@@ -0,0 +1,37 @@
+module Api
+  module V1
+    class TransfersController < ApplicationController
+      def create
+        transfer = Transfer.new(transfer_params)
+        if transfer.valid?
+          begin
+            transfer.save!
+            apply_exchange(transfer)
+          rescue ActiveRecord::RecordInvalid => e
+            render json: e
+          end
+        else
+          render json: transfer.errors.full_messages
+        end
+      end
+
+      private
+
+      def transfer_params
+        params.permit(:source_account_id, :destination_account_id, :amount)
+      end
+
+      def apply_exchange(transfer)
+        source_account = Account.find(transfer.source_account_id)
+        destination_account = Account.find(transfer.destination_account_id)
+        Account.transaction do
+          source_account.balance -= transfer.amount
+          destination_account.balance += transfer.amount
+
+          source_account.save!
+          destination_account.save!
+        end
+      end
+    end
+  end
+end
--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -3,7 +3,9 @@ class Transfer < ApplicationRecord
             :destination_account_id,
             :amount,
             presence: true, numericality: true
-  validate :source_account_presence, :destination_account_presence
+  validate :source_account_presence,
+           :destination_account_presence,
+           :source_account_has_enough_balance
 
   private
 
@@ -17,5 +19,14 @@ class Transfer < ApplicationRecord
     return if Account.exists?(destination_account_id)
 
     errors.add(:destination_account_id, 'does not exists')
+  end
+
+  def source_account_has_enough_balance
+    return unless Account.exists?(source_account_id)
+
+    source_balance = Account.find(source_account_id).balance
+    return if source_balance >= amount
+
+    errors.add(:source_account_id, 'has insufficient funds')
   end
 end
